8c2dadfa9ce37189284769abb561d9d9d4c73e5d
Yusha D <jojomemes@yandex.ru>
2022-03-06 16:42:00 +0400

    commit

--- бу/сет17.cpp
+++ бу/сет17.cpp
@@ -1,48 +1,51 @@
 #include <iostream>
 #include <set>
-#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void show(set<int> a) {
-	for (auto x : a)
-		cout << x << ' ';
-	cout << endl << endl;
+void insert(vector<int> &num, int n) {
+    for (int i = 0; i < n; i++) {
+        int m;
+        cin >> m;
+        num.push_back(m);
+    }
 }
 
-void insert(set<int> &a, int n) {
-	int num;
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		a.insert(num);
-	}
+void show(vector<int> a) {
+    for (auto x : a)
+        cout << x << ' ';
+}
+
+set<int> trans(int num) {
+    set<int> a;
+    while (num > 0) {
+        a.insert(num % 10);
+        num /= 10;
+    }
+    return a;
 }
 
 int main() {
+    
+    int n;
+    cin >> n;
 
-	int n;
-	set<int> mass;
+    vector<int> num;
+    insert(num, n);
 
-	cin >> n;
+    for (int i = 0; i < num.size()-1; i++) {
+        set<int> a = trans(num[i]);
+        set<int> b = trans(num[i+1]);
+        vector<int> ans;
 
-	insert(mass, n);
-	show(mass);
-	
-	for (auto it = mass.begin(); it != --mass.end(); it++) {
+        set_difference(a.begin(), a.end(),
+            b.begin(), b.end(), back_inserter(ans));
 
-		auto it2 = it;
-		it2++;
-
-		string s = to_string(*it);
-		string s2 = to_string(*it2);
-		string ans = "";
-
-		for (int i = 0; i < s.size(); i++)
-			if (s2.find(s[i]) == string::npos)
-				ans.insert(ans.size(), 1, s[i]);
-
-		(ans != "") ? (cout << ans << endl) : (cout << "none" << endl);
-
-	}
-
+        if (!ans.empty()) show(ans);
+        else cout << "none";
+        cout << endl;
+    }
+    
 }
